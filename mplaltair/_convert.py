c90083266cfa314c8a0d861268f9bf2f7dfbe996
kdorr <kdorr@users.noreply.github.com>
2018-06-27 18:42:18 -0500

    New approach for organization

--- mplaltair/_convert.py
+++ mplaltair/_convert.py
@@ -1,10 +1,15 @@
 from altair.utils.core import parse_shorthand
 from altair.utils.schemapi import Undefined
 
-_mappings = {
-    'x': lambda e, d: _process_x(e, d),
-    'y': lambda e, d: _process_y(e, d),
-    'color': lambda e, d: _process_color(e, d)
+
+# mapping: altair channel: mpl kwargs for scatter() (and plot(), sort of)
+_mpl_equivalent = {
+    'x': 'x',
+    'y': 'y',
+    'color': 'c',
+    'size': 's',
+    'opacity': 'alpha',
+    'shape': 'marker'
 }
 
 
@@ -27,38 +32,18 @@ def convert_quantitative(chart):
 
     mapping = {}
 
-    for channel in chart.to_dict()['encoding']:  # I was unable to get a key-value mapping from only the encoding
-        mpl_equivalent, converted_data = _mappings[channel](chart.encoding, chart.data)
-        mapping[mpl_equivalent] = converted_data
+    for encoding_channel in chart.to_dict()['encoding']:  # Need chart to get dictionary of channels from the encoding
+
+        channel = chart.encoding[encoding_channel]
+
+        if channel.field is not Undefined:
+            data = chart.data[channel.field].values
+        elif parse_shorthand(channel.shorthand, chart.data)['field'] is not Undefined:
+            data = chart.data[parse_shorthand(channel.shorthand, chart.data)['field']].values
+        else:
+            # TODO fail nicely
+            data = []
+
+        mapping[_mpl_equivalent[encoding_channel]] = data
 
     return mapping
-
-
-def _process_x(encoding, data):
-    if encoding.x.field is not Undefined:
-        return "x", data[encoding.x.field].values
-
-    if parse_shorthand(encoding.x.shorthand, data)['field']:
-        return "x", data[parse_shorthand(encoding.x.shorthand, data)['field']].values
-
-    return "x", None
-
-
-def _process_y(encoding, data):
-    if encoding.x.field is not Undefined:
-        return "y", data[encoding.y.field].values
-
-    if parse_shorthand(encoding.y.shorthand, data)['field']:
-        return "y", data[parse_shorthand(encoding.y.shorthand, data)['field']].values
-
-    return "y", None
-
-
-def _process_color(encoding, data):
-    if encoding.color.field is not Undefined:
-        return "c", data[encoding.color.field].values
-
-    if parse_shorthand(encoding.color.shorthand, data)['field']:
-        return "c", data[parse_shorthand(encoding.color.shorthand, data)['field']].values
-
-    return "c", None
