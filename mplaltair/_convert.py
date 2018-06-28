9b1037286cb82315979f72afa7a6bd41175d800d
kdorr <kdorr@users.noreply.github.com>
2018-06-28 17:02:02 -0500

    Add locate data function

--- mplaltair/_convert.py
+++ mplaltair/_convert.py
@@ -3,7 +3,7 @@ from altair.utils.schemapi import Undefined
 
 
 # mapping: altair channel: mpl kwargs for scatter() (and plot(), sort of)
-_mpl_equivalent = {
+_mpl_scatter_equivalent = {
     'x': 'x',
     'y': 'y',
     'color': 'c',
@@ -33,17 +33,28 @@ def convert_quantitative(chart):
     mapping = {}
 
     for encoding_channel in chart.to_dict()['encoding']:  # Need chart to get dictionary of channels from the encoding
-
         channel = chart.encoding[encoding_channel]
-
-        if channel.field is not Undefined:
-            data = chart.data[channel.field].values
-        elif parse_shorthand(channel.shorthand, chart.data)['field'] is not Undefined:
-            data = chart.data[parse_shorthand(channel.shorthand, chart.data)['field']].values
-        else:
-            # TODO fail nicely
-            data = []
-
-        mapping[_mpl_equivalent[encoding_channel]] = data
+        data = _locate_channel_data(channel, chart.data)
+        mapping[_mpl_scatter_equivalent[encoding_channel]] = data
 
     return mapping
+
+
+def _locate_channel_data(channel, data):
+    if hasattr(channel, "value") and channel.value is not Undefined:
+        return channel.value
+    elif hasattr(channel, "aggregate") and channel.aggregate is not Undefined:
+        return _aggregate_channel()
+    elif hasattr(channel, "field") and channel.field is not Undefined:
+        return data[channel.field].values
+    elif hasattr(channel, "shorthand") and channel.shorthand is not Undefined:
+        parsed = parse_shorthand(channel.shorthand, data)
+        if "field" in parsed:
+            return data[parsed['field']].values
+    else:
+        raise ValueError("Cannot find data for the channel")
+
+
+def _aggregate_channel():
+    # TODO implement proper handling of aggregate functions
+    return []
--- mplaltair/tests/test_convert.py
+++ mplaltair/tests/test_convert.py
@@ -6,35 +6,64 @@ import numpy as np
 import mplaltair._convert as convert
 import pytest
 
-df_basic = pd.DataFrame({"a": [1, 2, 3], "b": [1.2, 2.4, 3.8], "c": [7, 5, 3]})
-chart_x = alt.Chart(df_basic).mark_point().encode(alt.X('a'))
-chart_x_y = alt.Chart(df_basic).mark_point().encode(alt.X('a'), alt.Y('b'))
-chart_color = alt.Chart(df_basic).mark_point().encode(alt.X('a'), alt.Y('b'), color=alt.Color('c:Q'))
+df_basic = pd.DataFrame({
+    "a": [1, 2, 3], "b": [1.2, 2.4, 3.8], "c": [7, 5, 3],
+    "s": [50, 100, 200.0], "alpha": [.1, .5, .8], "shape": [1, 2, 3]
+})
+chart_x_y = alt.Chart(df_basic).mark_point().encode(alt.X(field='a', type='quantitative'), alt.Y('b'))
+chart_opacity = alt.Chart(df_basic).mark_point().encode(
+    alt.X(field='a', type='quantitative'), alt.Y('b'), alt.Color('c:Q'), opacity=alt.value(.5)
+)
+chart_shape = alt.Chart(df_basic).mark_point().encode(
+    alt.X(field='a', type='quantitative'), alt.Y('b'), alt.Color('c:Q'), alt.Shape('shape')
+)
+chart_all = alt.Chart(df_basic).mark_point().encode(
+    alt.X(field='a', type='quantitative'), alt.Y('b'), alt.Color('c:Q'), alt.Size('s')
+)
 
 df_count = pd.DataFrame({"a": [1, 1, 2, 3, 5], "b": [1.4, 1.4, 2.9, 3.18, 5.3]})
 chart_x_count_y = alt.Chart(df_count).mark_point().encode(alt.X('a'), alt.Y('count()'))
 
 
-def test_quantitative_x():
-    mapping = convert.convert_quantitative(chart_x)
-    assert np.array_equal(mapping['x'], df_basic['a'].values)
-
-
 def test_quantitative_x_y():
     mapping = convert.convert_quantitative(chart_x_y)
-    assert np.array_equal(mapping['x'], df_basic['a'].values)
-    assert np.array_equal(mapping['y'], df_basic['b'].values)
+    assert list(mapping['x']) == list(df_basic['a'].values)
+    assert list(mapping['y']) == list(df_basic['b'].values)
 
 
 def test_quantitative_color():
-    mapping = convert.convert_quantitative(chart_color)
-    assert np.array_equal(mapping['x'], df_basic['a'].values)
-    assert np.array_equal(mapping['y'], df_basic['b'].values)
-    assert np.array_equal(mapping['c'], df_basic['c'].values)
+    mapping = convert.convert_quantitative(chart_all)
+    # assert np.array_equal(mapping['c'], df_basic['c'].values)
+    assert list(mapping['c']) == list(df_basic['c'].values)
 
 
-def test_quantitative_scatter():
-    mapping = convert.convert_quantitative(chart_color)
+def test_quantitative_opacity():
+    mapping = convert.convert_quantitative(chart_opacity)
+    assert mapping['alpha'] == 0.5
+
+
+def test_quantitative_size():
+    mapping = convert.convert_quantitative(chart_all)
+    assert list(mapping['s']) == list(df_basic['s'].values)
+
+
+def test_quantitative_shape():
+    mapping = convert.convert_quantitative(chart_shape)
+    assert list(mapping['marker']) == list(df_basic['shape'].values)
+
+
+@pytest.mark.parametrize("chart,mpl,expected", [
+    (chart_all, 'x', df_basic['a'].values), (chart_all, 'y', df_basic['b'].values),
+    (chart_all, 'c', df_basic['c'].values), (chart_all, 's', df_basic['s'].values)
+])
+def test_quantitative_everything(chart, mpl, expected):
+    mapping = convert.convert_quantitative(chart)
+    assert list(mapping[mpl]) == list(expected)
+
+
+@pytest.mark.parametrize("chart", [chart_all, chart_opacity, chart_shape])
+def test_quantitative_scatter(chart):
+    mapping = convert.convert_quantitative(chart)
     plt.scatter(**mapping)
     plt.show()
 
