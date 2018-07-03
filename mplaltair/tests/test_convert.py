698d16651fdee0a4a6a12fad37e65a79e3525249
Nabarun Pal <pal.nabarun95@gmail.com>
2018-07-03 17:41:45 +0530

    Adds tests for y encoding channel

--- mplaltair/tests/test_convert.py
+++ mplaltair/tests/test_convert.py
@@ -27,3 +27,23 @@ def test_convert_x_fail():
     chart_spec = json.loads(alt.Chart(df).encode(x='b:N').mark_point().to_json())
     with pytest.raises(KeyError):
         convert(chart_spec)
+
+def test_convert_y_success_quantitative():
+    chart_spec = json.loads(alt.Chart(df).encode(y='quant').mark_point().to_json())
+    mapping = convert(chart_spec)
+    assert list(mapping['y']) == list(df.quant.values)
+
+def test_convert_y_success_ordinal():
+    chart_spec = json.loads(alt.Chart(df).encode(y='ord').mark_point().to_json())
+    mapping = convert(chart_spec)
+    assert list(mapping['y']) == list(df.ord.values)
+
+def test_convert_y_success_nominal():
+    chart_spec = json.loads(alt.Chart(df).encode(y='nom').mark_point().to_json())
+    mapping = convert(chart_spec)
+    assert list(mapping['y']) == list(df.nom.values)
+
+def test_convert_y_fail():
+    chart_spec = json.loads(alt.Chart(df).encode(y='b:N').mark_point().to_json())
+    with pytest.raises(KeyError):
+        convert(chart_spec)
