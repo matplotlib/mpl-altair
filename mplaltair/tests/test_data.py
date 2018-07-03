7664992a4235f8b8ac88086f888850ef7bd45949
Nabarun Pal <pal.nabarun95@gmail.com>
2018-07-03 12:07:49 +0530

    Adds tests for the data module

---
+++ mplaltair/tests/test_data.py
@@ -0,0 +1,35 @@
+import pytest
+import pandas as pd
+from vega_datasets import data
+from .._data import _normalize_data
+
+def test_data_list():
+    spec = {
+        "data": {
+            "values": [{"a": 1, "b": 2}, {"c": 3, "d": 4}]
+
+        }
+    }
+    assert type(_normalize_data(spec)["data"]) == pd.DataFrame
+
+def test_data_url():
+    spec = {
+        "data": {
+            "url": data.cars.url
+        }
+    }
+    assert type(_normalize_data(spec)["data"]) == pd.DataFrame
+
+def test_data_no_pass():
+    spec = {}
+    with pytest.raises(KeyError):
+        _normalize_data(spec)
+
+def test_data_invalid():
+    spec = {
+        "data": {
+            "source": "path"
+        }
+    }
+    with pytest.raises(NotImplementedError):
+        _normalize_data(spec)
