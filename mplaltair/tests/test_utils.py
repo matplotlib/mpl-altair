3c5e415546ff3e477196d74acfc53c68d38a77b6
Nabarun Pal <pal.nabarun95@gmail.com>
2018-07-03 12:08:58 +0530

    Adds tests for the _utils module

---
+++ mplaltair/tests/test_utils.py
@@ -0,0 +1,14 @@
+import pytest
+import pandas as pd
+from vega_datasets import data
+from .. import _utils
+
+def test_get_format():
+    assert _utils._get_format(data.cars.url) == 'json'
+
+def test_fetch_success():
+    assert type(_utils._fetch(data.cars.url)) == pd.DataFrame
+
+def test_fetch_error():
+    with pytest.raises(NotImplementedError):
+        _utils._fetch('https://test.tld/dataset.tsv')
