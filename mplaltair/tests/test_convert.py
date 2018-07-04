c78e2429f5791121842aaf313b3dd8ad5b4bba2e
kdorr <kdorr@users.noreply.github.com>
2018-07-04 10:25:40 -0500

    Clean up tests

--- mplaltair/tests/test_convert.py
+++ mplaltair/tests/test_convert.py
@@ -16,69 +16,65 @@ chart_all = alt.Chart(df_basic).mark_point().encode(
 chart_fill = alt.Chart(df_basic).mark_point().encode(
     alt.X(field='a', type='quantitative'), alt.Y('b'), alt.Fill('fill:Q')
 )
-# Charts for channels that aren't implemented yet
-chart_x2_y2_fail = alt.Chart(df_basic).mark_point().encode(
-    alt.X('a'), alt.Y('b'), alt.X2('c'), alt.Y2('alpha')
-)
-chart_opacity_fail = alt.Chart(df_basic).mark_point().encode(
-    alt.X(field='a', type='quantitative'), alt.Y('b'), alt.Opacity('alpha')
-)
-chart_shape_fail = alt.Chart(df_basic).mark_point().encode(
-    alt.X(field='a', type='quantitative'), alt.Y('b'), alt.Color('c:Q'), alt.Shape('shape')
-)
-chart_stroke_fail = alt.Chart(df_basic).mark_point().encode(
-    alt.X('a'), alt.Y('b'), alt.Stroke('fill')
-)
 
 df_count = pd.DataFrame({"a": [1, 1, 2, 3, 5], "b": [1.4, 1.4, 2.9, 3.18, 5.3]})
 chart_x_count_y = alt.Chart(df_count).mark_point().encode(alt.X('a'), alt.Y('count()'))
 
 
 def test_quantitative_x_y():
-    mapping = convert.convert_quantitative(chart_all)
+    chart = alt.Chart(df_basic).mark_point().encode(alt.X(field='a', type='quantitative'), alt.Y('b'))
+    mapping = convert.convert_quantitative(chart)
     assert list(mapping['x']) == list(df_basic['a'].values), "x values did not match expected x values"
     assert list(mapping['y']) == list(df_basic['b'].values), "y values did not match expected y values"
 
 
 @pytest.mark.xfail(raises=NotImplementedError, reason="It doesn't make sense to have x2 and y2 on scatter plots")
 def test_quantitative_x2_y2():
-    convert.convert_quantitative(chart_x2_y2_fail)
+    chart = alt.Chart(df_basic).mark_point().encode(alt.X('a'), alt.Y('b'), alt.X2('c'), alt.Y2('alpha'))
+    convert.convert_quantitative(chart)
 
 
 def test_quantitative_color():
-    mapping = convert.convert_quantitative(chart_all)
+    chart = alt.Chart(df_basic).mark_point().encode(alt.Color('c:Q'))
+    mapping = convert.convert_quantitative(chart)
     assert list(mapping['c']) == list(df_basic['c'].values)
 
 
 def test_quantitative_fill():
-    mapping = convert.convert_quantitative(chart_fill)
+    chart = alt.Chart(df_basic).mark_point().encode(alt.Fill('fill:Q'))
+    mapping = convert.convert_quantitative(chart)
     assert list(mapping['c']) == list(df_basic['fill'].values)
 
 
 def test_quantitative_opacity_value():
-    mapping = convert.convert_quantitative(chart_all)
+    chart = alt.Chart(df_basic).mark_point().encode(opacity=alt.value(.5))
+    mapping = convert.convert_quantitative(chart)
     assert mapping['alpha'] == 0.5
 
 
 @pytest.mark.xfail(raises=NotImplementedError, reason="The alpha argument in scatter() cannot take arrays")
 def test_quantitative_opacity_array():
-    convert.convert_quantitative(chart_opacity_fail)
+    chart = alt.Chart(df_basic).mark_point().encode(alt.Opacity('alpha'))
+    convert.convert_quantitative(chart)
 
 
 @pytest.mark.xfail(raises=NotImplementedError, reason="The marker argument in scatter() cannot take arrays")
 def test_quantitative_shape():
-    mapping = convert.convert_quantitative(chart_shape_fail)
+    chart = alt.Chart(df_basic).mark_point().encode(alt.Shape('shape'))
+    mapping = convert.convert_quantitative(chart)
     assert list(mapping['marker']) == list(df_basic['shape'].values)
 
 
 def test_quantitative_size():
-    mapping = convert.convert_quantitative(chart_all)
+    chart = alt.Chart(df_basic).mark_point().encode(alt.Size('s'))
+    mapping = convert.convert_quantitative(chart)
     assert list(mapping['s']) == list(df_basic['s'].values)
 
 
 @pytest.mark.xfail(raises=NotImplementedError, reason="Stroke is not well supported in Altair")
 def test_quantitative_stroke():
-    convert.convert_quantitative(chart_stroke_fail)
+    chart = alt.Chart(df_basic).mark_point().encode(alt.Stroke('fill'))
+    convert.convert_quantitative(chart)
 
 
 @pytest.mark.parametrize("chart", [chart_all, chart_fill])
@@ -90,6 +86,7 @@ def test_quantitative_scatter(chart):
 
 @pytest.mark.xfail(raises=NotImplementedError, reason="Aggregate functions are not supported yet")
 def test_quantitative_x_count_y():
-    mapping = convert.convert_quantitative(chart_x_count_y)
+    chart = alt.Chart(df_count).mark_point().encode(alt.X('a'), alt.Y('count()'))
+    mapping = convert.convert_quantitative(chart)
     assert list(mapping['x']) == list(df_count['a'].values)
     assert list(mapping['y']) == list(df_count.groupby(['a']).count().values)
