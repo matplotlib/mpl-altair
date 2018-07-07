94c3193d8efcd12ae2d79567f749f6ca13f8df5a
Nabarun Pal <pal.nabarun95@gmail.com>
2018-07-07 10:58:38 +0530

    Refactors tests to use pytest.marks.parametrize

--- mplaltair/tests/test_convert.py
+++ mplaltair/tests/test_convert.py
@@ -7,55 +7,33 @@ from .._convert import convert
 
 df = pd.DataFrame({'quant': [1, 1.5, 2], 'ord': [0, 1, 2], 'nom': ['A', 'B', 'C']})
 
-def test_convert_x_success_quantitative():
-    chart_spec = alt.Chart(df).encode(x='quant').mark_point().to_dict()
+@pytest.mark.parametrize('channel', ['quant', 'ord', 'nom'])
+def test_convert_x_success(channel):
+    chart_spec = alt.Chart(df).encode(x=channel).mark_point().to_dict()
     mapping = convert(chart_spec)
-    assert list(mapping['x']) == list(df.quant.values)
-
-def test_convert_x_success_ordinal():
-    chart_spec = alt.Chart(df).encode(x='ord').mark_point().to_dict()
-    mapping = convert(chart_spec)
-    assert list(mapping['x']) == list(df.ord.values)
-
-def test_convert_x_success_nominal():
-    chart_spec = alt.Chart(df).encode(x='nom').mark_point().to_dict()
-    mapping = convert(chart_spec)
-    assert list(mapping['x']) == list(df.nom.values)
+    assert list(mapping['x']) == list(df[channel].values)
 
 def test_convert_x_fail():
     chart_spec = alt.Chart(df).encode(x='b:N').mark_point().to_dict()
     with pytest.raises(KeyError):
         convert(chart_spec)
 
-def test_convert_y_success_quantitative():
-    chart_spec = alt.Chart(df).encode(y='quant').mark_point().to_dict()
+@pytest.mark.parametrize('channel', ['quant', 'ord', 'nom'])
+def test_convert_y_success(channel):
+    chart_spec = alt.Chart(df).encode(y=channel).mark_point().to_dict()
     mapping = convert(chart_spec)
-    assert list(mapping['y']) == list(df.quant.values)
-
-def test_convert_y_success_ordinal():
-    chart_spec = alt.Chart(df).encode(y='ord').mark_point().to_dict()
-    mapping = convert(chart_spec)
-    assert list(mapping['y']) == list(df.ord.values)
-
-def test_convert_y_success_nominal():
-    chart_spec = alt.Chart(df).encode(y='nom').mark_point().to_dict()
-    mapping = convert(chart_spec)
-    assert list(mapping['y']) == list(df.nom.values)
+    assert list(mapping['y']) == list(df[channel].values)
 
 def test_convert_y_fail():
     chart_spec = alt.Chart(df).encode(y='b:N').mark_point().to_dict()
     with pytest.raises(KeyError):
         convert(chart_spec)
 
-def test_convert_color_success_quantitative():
-    chart_spec = alt.Chart(df).encode(color='quant').mark_point().to_dict()
+@pytest.mark.parametrize('channel', ['quant', 'ord'])
+def test_convert_color_success(channel):
+    chart_spec = alt.Chart(df).encode(color=channel).mark_point().to_dict()
     mapping = convert(chart_spec)
-    assert list(mapping['c']) == list(df.quant.values)
-
-def test_convert_color_success_ordinal():
-    chart_spec = alt.Chart(df).encode(color='ord').mark_point().to_dict()
-    mapping = convert(chart_spec)
-    assert list(mapping['c']) == list(df.ord.values)
+    assert list(mapping['c']) == list(df[channel].values)
 
 def test_convert_color_success_nominal():
     chart_spec = alt.Chart(df).encode(color='nom').mark_point().to_dict()
@@ -67,15 +45,11 @@ def test_convert_color_fail():
     with pytest.raises(KeyError):
         convert(chart_spec)
 
-def test_convert_fill_success_quantitative():
-    chart_spec = alt.Chart(df).encode(fill='quant').mark_point().to_dict()
+@pytest.mark.parametrize('channel', ['quant', 'ord'])
+def test_convert_fill(channel):
+    chart_spec = alt.Chart(df).encode(fill=channel).mark_point().to_dict()
     mapping = convert(chart_spec)
-    assert list(mapping['c']) == list(df.quant.values)
-
-def test_convert_fill_success_ordinal():
-    chart_spec = alt.Chart(df).encode(fill='ord').mark_point().to_dict()
-    mapping = convert(chart_spec)
-    assert list(mapping['c']) == list(df.ord.values)
+    assert list(mapping['c']) == list(df[channel].values)
 
 def test_convert_fill_success_nominal():
     chart_spec = alt.Chart(df).encode(fill='nom').mark_point().to_dict()
@@ -87,15 +61,11 @@ def test_convert_fill_fail():
     with pytest.raises(KeyError):
         convert(chart_spec)
 
-def test_convert_size_success_quantitative():
-    chart_spec = alt.Chart(df).encode(size='quant').mark_point().to_dict()
+@pytest.mark.parametrize('channel', ['quant', 'ord'])
+def test_convert_size_success(channel):
+    chart_spec = alt.Chart(df).encode(size=channel).mark_point().to_dict()
     mapping = convert(chart_spec)
-    assert list(mapping['s']) == list(df.quant.values)
-
-def test_convert_size_success_ordinal():
-    chart_spec = alt.Chart(df).encode(size='ord').mark_point().to_dict()
-    mapping = convert(chart_spec)
-    assert list(mapping['s']) == list(df.ord.values)
+    assert list(mapping['s']) == list(df[channel].values)
 
 def test_convert_size_success_nominal():
     chart_spec = alt.Chart(df).encode(size='nom').mark_point().to_dict()
