87142fa33a6b4ebee0ff96687012cc408a1f57ee
Nabarun Pal <pal.nabarun95@gmail.com>
2018-08-08 15:38:39 +0530

    Adds data normalizer to _data.py

--- mplaltair/_data.py
+++ mplaltair/_data.py
@@ -4,6 +4,47 @@ import matplotlib.cbook as cbook
 from datetime import datetime
 import numpy as np
 
+import pandas as pd
+
+from ._utils import _fetch
+
+def _normalize_data(spec):
+    """Converts the data to a Pandas dataframe
+
+    Parameters
+    ----------
+    spec : dict
+    The vega-lite specification in json format
+
+    Returns
+    -------
+    dict
+    The vega-lite specification with the data format fixed to a Pandas dataframe
+
+    Raises
+    ------
+    ValidationError
+    Raised when the specification does not contain any data attribute
+
+    NotImplementedError
+    Raised when the data specification has an unsupported data source
+    """
+
+    if not spec.get('data'):
+        raise ValidationError('Please specify a data source.')
+    
+    if spec['data'].get('url'):
+        df = pd.DataFrame(_fetch(spec['data']['url']))
+    elif spec['data'].get('values'):
+        df = pd.DataFrame(spec['data']['values'])
+    else:
+        raise NotImplementedError('Given data specification is unsupported at the moment.')
+
+    del spec['data']
+    spec['data'] = df
+
+    return spec
+
 def _locate_channel_dtype(chart, channel):
     """Locates dtype used for each channel
         Parameters
